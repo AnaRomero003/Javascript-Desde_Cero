1f456ff0fbef55c40fedcbd5f3a8624a03f153e8
AR <20220901@uthh.edu.mx>
2025-02-04 20:31:28 -0600

    Tema: Tipos de datos

---
+++ tipos-de-datos.js
@@ -0,0 +1,51 @@
+//Este es un comentario
+
+//Console.log es una funcion que se utiliza para imprimir en consola
+console.log("Hola mundo");
+
+//Declaracion devariables y asignacion de valores
+var nombre = "Ana"; //String
+var edad = 23; //Number
+var sueldo = 1.234; //Number
+var casada = false //Boolean
+var vacunas = true //Boolean
+var hijos = null //Null
+var dato; //Undefind
+
+//typeof se utiliza para saber el tipo de dato de una variable
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(sueldo));
+console.log(typeof(casada));
+console.log(typeof(vacunas));
+console.log(typeof(hijos));
+console.log(typeof(dato));
+
+//La semantica es importante, significado de las palabras
+var nombre = "Francia";
+var pais = 20;
+
+//NaN (Not a Number) es un valor especial que indica que una operacion matematica no es un numero.
+console.log(10*"Ana");
+
+//Si ponemos typeof de null dira que es objeto, pero en realidad es ausencia intencional de valor. Esto es debido a un error de Javascript que no se ha corregido por compatibilidad con versionesanteriores
+
+/*Comentarios en bloque o multilinea*/
+
+//Ejercicios de CAMPUS
+console.log(typeof(42));
+console.log(typeof('Veinticinco'));
+
+console.log(typeof(-666));
+console.log(typeof(true));
+console.log(typeof(0));
+console.log(typeof('"'));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(FALSE));
+
+console.log(typeof(24.66));
+console.log(typeof(false));
+console.log(typeof('Cristina'));
+console.log(typeof(null));
+console.log(typeof(-78.4));

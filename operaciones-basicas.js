7c1d186afc2c5e7ab69ae65a9aa8b8d9cafa712c
AR <20220901@uthh.edu.mx>
2025-02-06 19:43:48 -0600

    Tema: Operadores logicos

--- operaciones-basicas.js
+++ operaciones-basicas.js
@@ -1,3 +1,4 @@
+//Declaracion de variables usando let
 let valor_1 = 27;
 let valor_2 = 4;
 
@@ -20,3 +21,48 @@ console.log(modulo);
 
 let exponencial = valor_1 ** valor_2;
 console.log(exponencial);
+
+//Operadores de comparacion
+
+let numero_1 = 3; //Numero
+let numero_2 = "3"; //String
+
+console.log(numero_1 == numero_2); //Comparacion debil
+
+console.log(numero_1 === numero_2); //Comparacion estricta
+
+console.log("Operadores logicos y de comparacion");
+
+console.log(10>4); //Mayor que
+console.log(2<1); //Menor que
+console.log(10>=10); //Mayor o igual que
+console.log(10<=10); //Menor o igual que
+
+// La respuesta en valorr booleano
+
+console.log("Diferente de");
+console.log(15 != 15); //False, porque es el mismo valor
+
+console.log("AND: &&");
+// Para poder entrar a un antro
+let edad = 20;
+let teneridentificacion = true;
+console.log(edad >= 18 && teneridentificacion);
+
+console.log("OR: ||");
+//Para aprender a programar
+let esMayorDeEdad = true;
+let esEstudiante = false;
+console.log(esMayorDeEdad || esEstudiante);
+
+console.log("NOT: !");
+let esFinDeSemana = true;
+console.log(!esFinDeSemana);
+
+let calificacion = 9;
+let calificacion1 = 10;
+let calificacion2 = 3;
+let calificacion3 = 7;
+let calificacion4 = 8;
+
+// += Nos permite hacer una suma acumulativa

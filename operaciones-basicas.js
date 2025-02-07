94c1f75e7fd44778d57fa56a7c2af42f90e7cfae
AR <20220901@uthh.edu.mx>
2025-02-06 19:08:54 -0600

    Operaciones basicas

---
+++ operaciones-basicas.js
@@ -0,0 +1,22 @@
+let valor_1 = 27;
+let valor_2 = 4;
+
+//Operaciones basicas
+
+let suma = valor_1 + valor_2;
+console.log(suma);
+
+let resta = valor_1 - valor_2;
+console.log(resta);
+
+let multiplicacion = valor_1 * valor_2;
+console.log(multiplicacion);
+
+let division = valor_1 / valor_2;
+console.log(division);
+
+let modulo = valor_1 % valor_2;
+console.log(modulo);
+
+let exponencial = valor_1 ** valor_2;
+console.log(exponencial);
---
+++ variables.js
@@ -0,0 +1,23 @@
+//var es una forma antigua de declarar variables, let es la forma moderna de declarar variables
+
+var nombre = "Ana";
+
+var nombre = "Fernanda";
+
+var nombre = "Victor";
+
+console.log(nombre);
+
+// let no permite redeclarar variables, lo que significa que no puedes declarar la misma variable dos veces
+
+//let tiene un scape de bloque, lo que significa que solo vive en el bloque donde fue declarado. Un bloque es {}
+
+let apellido1 = "Gutierrez";
+
+let apellido2 = "Lopez";
+
+// let si te permite cambiar el valor
+//Reasignacion de valor
+
+apellido1 = "Perez";
+console.log(apellido1);
